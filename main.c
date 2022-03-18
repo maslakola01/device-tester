2c7273f52c6e856c8861520eef9615aba07dfde1
maslakola01 <aleksandra.maslak1@gmail.com>
2022-03-18 09:35:58 +0100

    buttons not working

--- main.c
+++ main.c
@@ -3,17 +3,13 @@
 #include <avr/iom32.h>	
 #include <util/delay.h>
 #include <avr/sleep.h>
-
 #include "lcd.h"
-
-
-
-#define F_CPU 8000000UL			/* Define CPU Frequency 8MHz */
 #include <avr/io.h>			/* Include AVR std. library file */
 #include <stdio.h>			/* Include Std. i/p o/p file */
 #include <string.h>			/* Include String header file */
+#include <unistd.h>
 
-
+#define F_CPU 8000000UL			/* Define CPU Frequency 8MHz */
 #define MOSI 5								/* Define SPI bus pins */
 #define MISO 6
 #define SCK 7
@@ -23,7 +19,6 @@
 
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
-
 
 
 /*CS for transducers*/
@@ -37,21 +32,41 @@
 #define START_TRANSITION_C7 PORTC &= ~(1<<PC7)
 
 
+#define INIT_SW_A DDRD &= ~(1<<PD2) //wejscie
+#define SW_CONFIG_PULLUP_A PORTD |= (1<<PD2) //wewnetrzny pullup
+
+#define INIT_SW_B DDRD &= ~(1<<PD3) //wejscie
+#define SW_CONFIG_PULLUP_B PORTD |= (1<<PD3) //wewnetrzny pullup
+
+#define INIT_SW_C DDRB &= ~(1<<PB2) //wejscie
+#define SW_CONFIG_PULLUP_C PORTB |= (1<<PB2) //wewnetrzny pullup
+
 uint8_t count1, count2, count3; // master value
 uint8_t count1_s, count2_s, count3_s; 
 volatile uint16_t sum_first_device = 0; // sum 0-8 
 volatile uint16_t sum_second_device = 0; //sum 8-16
+volatile uint8_t channel_i = 1; //choose cgannel
+volatile uint8_t start_program = 0; 
+volatile uint8_t accept = 0;
+volatile int c = 0; // data from python 
 
 
 ISR(INT0_vect){
 
-    //guzik  pierwszy - potwierdzenie lvlu
-
+    channel_i++;
+    if(channel_i == 10){
+        channel_i = 1;
+    }
 }
+
 ISR(INT1_vect){
 
-    //guzik drugi
+    start_program++;
+}
 
+ISR(INT2_vect){
+
+    accept++;
 }
 
 void UART_init()
@@ -72,8 +87,6 @@ void SPI_Init_Master()
 						
 	SS_Disable;
 	SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0);	//  Fosc/16
-					
-
 }
 
 
@@ -84,7 +97,6 @@ char SPI_Write_Master(char data)		/* SPI write data function */
 	while(!(SPSR & (1<<SPIF)));	/* Czekanie na koniec transmisji */
     a=SPDR;
 	return a;		/* Skopiowanie danych */
-
 }
 
 
@@ -174,12 +186,10 @@ uint8_t change_byte(uint8_t number){
 }
 
 void UART_SendString(uint16_t number)
-{	    
-    
-         UART_TxChar(number >> 8);
-        
-		 UART_TxChar(number & 0x00FF);
-         UART_TxChar('\n');
+{	     
+    UART_TxChar(number >> 8);
+	UART_TxChar(number & 0x00FF);
+    UART_TxChar('\n');
 }
 
 void FirstDeviceEnable()
@@ -305,19 +315,69 @@ char askLevel(int lvl){
     }
 }
 
+void lcd_option(int b){
+
+    char buffer1[2];
+    sprintf(buffer1, "%d", channel_i);    
 
 
-int main(int arc, char *argv[]){
+    //0
+    if(b==48){
+                
+        LCD_String("    Hello " );	/* Write string on 1st line of LCD*/
+        LCD_Command(0xC0);		/* Go to 2nd line*/
+        LCD_String("     user! ");	/* Write string on 2nd line*/
+    } 
+    //1
+    if(b==49){
+                
+        LCD_String(" Waiting for  " );	/* Write string on 1st line of LCD*/
+        LCD_Command(0xC0);		/* Go to 2nd line*/
+        LCD_String("    device...");	/* Write string on 2nd line*/
+    } 
+    //2
+    if(b==50){
+                
+        LCD_String("   Device " );	/* Write string on 1st line of LCD*/
+        LCD_Command(0xC0);		/* Go to 2nd line*/
+        LCD_String(" is working ");	/* Write string on 2nd line*/
+    }
+    
+    //3
+    if(b==51){
+         
+        LCD_String(" Choose lvl:" );	/* Write string on 1st line of LCD*/
+        LCD_Command(0xC0);		/* Go to 2nd line*/
+        LCD_String(buffer1);	/* Write string on 2nd line*/
+    } 
+    //4
+    if(b==52){
+             
+        LCD_String("You chose lvl:" );	/* Write string on 1st line of LCD*/
+        LCD_Command(0xC0);		/* Go to 2nd line*/
+        LCD_String(buffer1);	/* Write string on 2nd line*/
+    } 
+
+}
 
 
-
+int main(){
 
     INIT_PC0;
     INIT_PC7;
+    INIT_SW_A;
+    INIT_SW_B;
+    INIT_SW_C;
     LCD_Init();			/* Initialization of LCD*/
     STOP_TRANSITION_C0;
     STOP_TRANSITION_C7;
+    SW_CONFIG_PULLUP_A;
+    SW_CONFIG_PULLUP_B;
+    SW_CONFIG_PULLUP_C;
 
+    sei();
+
+    
     /*generacja przerwan - guziki*/
 	MCUCR |= ((1<<ISC01) | (1<<ISC11)); //sposob generacji przerwania
     MCUCSR |=(1<<ISC2);
@@ -347,35 +407,41 @@ int main(int arc, char *argv[]){
    UART_TxChar(0xFF); // stop bit
    UART_TxChar('\n');    
 
-    int c = UART_RxChar();
+    lcd_option(48); //hello user
+    _delay_ms(5000);
+    LCD_Clear();
+
+    lcd_option(49); //waiting for device
+    _delay_ms(5000);
+
+    //while(start_program == 0);
+
+    c = UART_RxChar();
+
+    LCD_Clear();
+    lcd_option(50); //device is working
+    _delay_ms(5000);
+    LCD_Clear();
+
+    lcd_option(51); //choose lvl   
+    _delay_ms(3000);
+    
+    while(accept == 0){
+        _delay_ms(1000);
+    }
+    
+    LCD_Clear();
+    lcd_option(52); 
+     _delay_ms(3000);
+
+    c = UART_RxChar();
     char buffer[8];
     sprintf(buffer, "%d", c);
 
 
-    
-
-       if(c==48){
-                
-            LCD_String("  Connected to " );	/* Write string on 1st line of LCD*/
-            LCD_Command(0xC0);		/* Go to 2nd line*/
-            LCD_String("     device");	/* Write string on 2nd line*/
-        } 
-        if(c==49){
-                
-            LCD_String("  Upload to " );	/* Write string on 1st line of LCD*/
-            LCD_Command(0xC0);		/* Go to 2nd line*/
-            LCD_String("     device");	/* Write string on 2nd line*/
-        } 
-        if(c==50){
-                
-            LCD_String(" Waiting " );	/* Write string on 1st line of LCD*/
-            LCD_Command(0xC0);		/* Go to 2nd line*/
-            LCD_String(" for device");	/* Write string on 2nd line*/
-        } 
-
-     
-
     while(1);
+
+    
 
     return 0; 
 
